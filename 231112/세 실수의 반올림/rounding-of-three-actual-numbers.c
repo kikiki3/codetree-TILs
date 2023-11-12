2fa50682c77fff0f1f48aa08d1f6523b12f21f7e
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2023-11-12 21:26:20 +0900

    Title: 세 실수의 반올림, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231112/세 실수의 반올림/rounding-of-three-actual-numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    double a;
+    double b;
+    double c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    printf("%.3lf\n%.3lf\n%.3lf\n", a, b, c);
+    return 0;
+}
