89d3b3c7836480e2bbc20b0b54bcb78799680884
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2023-12-12 22:47:03 +0900

    Title: 숫자의 조건 여부, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231212/숫자의 조건 여부/number's-condition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    scanf("%d", &a);
+    if (a >= 113) {
+        int b = 1;
+        prinf("%d", b);
+    }
+    else {
+        int c = 0;
+        printf("%d", c);
+    }
+}
