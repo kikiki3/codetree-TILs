7bb47ad2f43c2c85e24316dfa73cb602b1a6f62a
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2023-12-22 12:22:47 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231222/코로나 메뉴얼/covid-manual.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int at, bt, ct;
+    char A, B, C;
+
+    scanf("%c %d\n%c %d\n%c %d", &A, &at, &B, &bt, &C, &ct);
+
+    if (A == 'Y' && B == 'Y' || B == 'Y' && C == 'Y' || A == 'Y' && C == 'Y') {
+        if (at >= 37 && bt >= 37 || bt >= 37 && ct >= 37 || at >= 37 && ct >= 37) {
+            printf("E");
+        }
+        
+    }
+    else {
+        printf("N");    }
+}
