a6fc1a27264cc7f4600fd41c424be91f411439d0
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2023-12-22 12:45:40 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231222/중앙값 구하기/find-the-median.c
+++ 231222/중앙값 구하기/find-the-median.c
@@ -4,11 +4,13 @@ int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
     
-    if ((a+b+c)/3 == a) {
+    if (a < b && a > c || a>b && a < c) {
         printf("%d", a);
     }
-    else if ((a+b+c) == b) {
+    else if (b < a && b > c || b > a && b < c) {
         printf("%d", b);
     }
-    else {printf("%d", c);}
+    else {
+        printf("%d", c);
+    }
 }
