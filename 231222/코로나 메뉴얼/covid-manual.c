6e70d81178950765aeeb022790cf2f463b8c216f
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2023-12-22 12:30:28 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231222/코로나 메뉴얼/covid-manual.c
+++ 231222/코로나 메뉴얼/covid-manual.c
@@ -6,11 +6,12 @@ int main() {
 
     scanf("%c %d\n%c %d\n%c %d", &A, &at, &B, &bt, &C, &ct);
 
-    if (A == 'Y' && B == 'Y' || B == 'Y' && C == 'Y' || A == 'Y' && C == 'Y') {
+    if (A == 'Y' && B == 'Y' || B == 'Y' && C == 'Y' || A == 'Y' && C == 'Y') 
+    {
         if (at >= 37 && bt >= 37 || bt >= 37 && ct >= 37 || at >= 37 && ct >= 37) {
             printf("E");
         }
-        
+        else {printf("N");}
     }
     else {
         printf("N");    }
