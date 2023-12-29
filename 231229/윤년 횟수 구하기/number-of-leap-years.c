195c60bed19ed6620e232069d1a467e880e7f0c0
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2023-12-29 22:37:47 +0900

    Title: 윤년 횟수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231229/윤년 횟수 구하기/number-of-leap-years.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n, cnt = 0;
+    scanf("%d", &n);
+
+    for (int i = 1; i < n; i++) {
+        if (i % 4 == 0 || i % 100 == 0 && i % 400 == 0) {
+            cnt++;
+        }
+    }
+    printf("%d", cnt);
+}
