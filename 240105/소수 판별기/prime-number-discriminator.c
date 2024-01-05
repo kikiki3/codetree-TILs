37678266609e048849addf6831aad3cd206902b0
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-01-05 16:01:15 +0900

    Title: 소수 판별기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/소수 판별기/prime-number-discriminator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int n;
+    bool satisfied = true;
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            satisfied = false;
+        }
+    }
+
+    if (satisfied == true) {
+        printf("P");
+    }
+    else {
+        printf("C");
+    }
+}
