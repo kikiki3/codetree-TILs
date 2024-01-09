4395ebe6957bc9b643904b255e656ecbc0f88a49
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-01-09 21:22:19 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240109/특정 규칙에 따른 문자 출력/character-output-according-to-rule.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = n - 1 - i; j > 0; j--) {
+            printf("  ");
+        }
+        for (int j = i+1; j > 0; j--) {
+            printf("@ ");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = n - 1 - i; j > 0; j--) {
+            printf("@ ");
+        }
+        printf("\n");
+    }
+}
