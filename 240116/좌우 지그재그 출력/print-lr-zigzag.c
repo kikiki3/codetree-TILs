43875695ed3e1d977980847dd15626303dfdaa9a
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-01-16 18:08:37 +0900

    Title: 좌우 지그재그 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240116/좌우 지그재그 출력/print-lr-zigzag.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int cnt = 0;
+    int dnt = 2*n+1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i % 2 == 0) {
+                cnt++;
+                printf("%d ", cnt);
+                
+                if (cnt == (i+1)*n) {
+                    cnt = cnt+(i+1)*n;
+                }
+            }
+            else if (i % 2 == 1) {
+                dnt--;
+                printf("%d ", dnt);
+                
+                if (dnt == i*(n+1)) {
+                    dnt = n*(i+1);
+                }
+            }
+        }
+        printf("\n");
+    }
+}
