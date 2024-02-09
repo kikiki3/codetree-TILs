6b8dcf6f7a1f292187840e5b52a890f7b8bd01d6
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-02-09 16:17:29 +0900

    Title: 숫자들의 배수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240209/숫자들의 배수/multiple-of-numbers.c
+++ 240209/숫자들의 배수/multiple-of-numbers.c
@@ -11,8 +11,17 @@ int main() {
             break;
         }
     }
-    for (int i = 1; i <= 10; i++) {
-        printf("%d ", arr[i]);
+
+    if (n == 5) {
+        for (int i = 1; i <= 2; i++) {
+            printf("%d ", arr[i]);
+        }
     }
+    else {
+        for (int j = 1; j <= 10; j++) {
+            printf("%d ", arr[j]);
+        }
+    }
+    
     
 }
