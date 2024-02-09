94670f15d70ac4820b7ab15295bc7300bfa51e2d
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-02-09 18:13:39 +0900

    Title: 전항의 두 배, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240209/전항의 두 배/twice-the-previous.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    int arr[10];
+
+    arr[0] = a;
+    arr[1] = b;
+
+    for (int i = 2; i < 10; i++) {
+        arr[i] = arr[i-1] + (2*arr[i-2]);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+}
