0db024378526ccec5c4050f8df6add298576e81a
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-02-24 01:04:39 +0900

    Title: 특정 위치의 문자, Time: 4ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240224/특정 위치의 문자/char-in-specific-location.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char dax[6] = {'L', 'E', 'B', 'R', 'O'};
+
+    int idx = -1;
+
+    char n;
+    scanf("%c", &n);
+
+    for (int i = 0; i < 6; i++) {
+        if (dax[i] == n) {
+            idx = i;
+        }
+    }
+
+    if (idx == 1) {
+        printf("None");
+    }
+    else {
+        printf("%d", idx);
+    }
+}
