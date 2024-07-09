7bb2bf8eaadbe7d3b1c578acace6501fae04b9b6
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-07-10 00:52:20 +0900

    Title: 500 근처의 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240710/500 근처의 수/near-500.c
+++ 240710/500 근처의 수/near-500.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 
 int main() {
-    //배열을 구현합니다.
-    int arr[10];
-    //10개의 정수를 입력받습니다.
-    for (int i = 0; i < 10; i++) {
-        scanf("%d", &arr[i]);
-    }
+    // 배열을 구현합니다.
+	int arr[10];
+	
+	// 10개의 정수를 입력받습니다.
+	for(int i = 0; i < 10; i++) {
+		scanf("%d", &arr[i]);
+	}
+	
+	// max_val : 500 미만의 가장 큰 수, min_val : 500 초과의 가장 작은 수
+	int max_val = 1;
+	int min_val = 1000;
+	
+	// 10개의 숫자들 중 500근처의 수들을 구합니다.
+	for(int i = 0; i < 10; i++) {
+		if(arr[i] < 500 && arr[i] > max_val)
+			max_val = arr[i];
+		
+		if(arr[i] > 500 && arr[i] < min_val)
+			min_val = arr[i];
+	}
+	
+    // 구한 값을 출력합니다.
+    printf("%d %d", max_val, min_val);
 
-    int max_val = 1;
-    int min_val = 1000;
-
-    for (int i = 0; i < 10; i++) {
-        if(arr[i] < 500 && arr[i] > max_val) {
-            max_val = arr[i];
-        }
-
-        if(arr[i] > 500 && arr[i] > min_val) {
-            min_val = arr[i];
-        }
-
-        printf("%d %d", max_val, min_val);
-    }
+    return 0;
 }
