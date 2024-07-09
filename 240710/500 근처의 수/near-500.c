95ee4344d76bc9ab6bcfc90a6760863c5992fe6a
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-07-10 00:51:13 +0900

    Title: 500 근처의 수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240710/500 근처의 수/near-500.c
+++ 240710/500 근처의 수/near-500.c
@@ -2,7 +2,7 @@
 
 int main() {
     //배열을 구현합니다.
-    int arr[10]
+    int arr[10];
     //10개의 정수를 입력받습니다.
     for (int i = 0; i < 10; i++) {
         scanf("%d", &arr[i]);
@@ -13,7 +13,7 @@ int main() {
 
     for (int i = 0; i < 10; i++) {
         if(arr[i] < 500 && arr[i] > max_val) {
-            max_val = arr[i]
+            max_val = arr[i];
         }
 
         if(arr[i] > 500 && arr[i] > min_val) {
