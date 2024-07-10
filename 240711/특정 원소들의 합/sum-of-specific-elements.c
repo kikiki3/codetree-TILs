e6ff9eb18b868d70c0d03cf86b0dfb970d833e69
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-07-11 00:29:20 +0900

    Title: 특정 원소들의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/특정 원소들의 합/sum-of-specific-elements.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int arr[4][4];
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            scanf("%d ", &arr[i][j]);
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j <= i; j++) {
+            sum += arr[i][j];
+        }
+    }
+    printf("%d", sum);
+}
