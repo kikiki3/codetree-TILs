1401929083590db8aad9a5977370246bd86ca609
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-07-11 12:58:45 +0900

    Title: 두 개의 격자 비교하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240711/두 개의 격자 비교하기/compare-two-grid.c
+++ 240711/두 개의 격자 비교하기/compare-two-grid.c
@@ -8,19 +8,19 @@ int main() {
     int arr_2[n][m];
     int arr_3[n][m];
 
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             scanf("%d ", &arr_1[i][j]);
         }
     }
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             scanf("%d ", &arr_2[i][j]);
         }
     }
 
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             if (arr_1[i][j]==arr_2[i][j]) {
                 arr_3[i][j] = 0;
             }
@@ -30,8 +30,8 @@ int main() {
         }
     }
 
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             printf("%d ", arr_3[i][j]);
         }
         printf("\n");
