e598bba47b5b6901f01568f93500e97b45a33845
kikiki3 <143614625+kikiki3@users.noreply.github.com>
2024-10-08 21:52:17 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241008/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void Print(int n) {
+    for (int i = 0; i < n; i++) {
+        printf("12345^&*()_\n");
+    }
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    Print(N);
+}
